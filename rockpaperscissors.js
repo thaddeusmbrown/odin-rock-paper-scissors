e4c83bf4c3eb7274c0d6b396f39d7b4e080a336d
thaddeusmbrown <thaddeusmbrown@gmail.com>
2021-12-20 15:07:53 -0600

    added playRound function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -9,3 +9,36 @@ function computerPlay() {
         return "scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection=playerSelection.toLowerCase();
+    if (playerSelection=="rock") {
+        if (computerSelection=="rock") {
+            return "Tie game! Rock ties Rock.";
+        } else if (computerSelection=="paper") {
+            return "You lose! Paper beats Rock";
+        } else {
+            return "You win! Rock beats Scissors";
+        }
+    } else if (playerSelection=="paper") {
+        if (computerSelection=="rock") {
+            return "You win! Paper beats Rock.";
+        } else if (computerSelection=="paper") {
+            return "You tie! Paper ties Paper";
+        } else {
+            return "You lose! Scissors beats Paper.";
+        }
+    } else {
+        if (computerSelection=="rock") {
+            return "You lose! Rock beats Scissors.";
+        } else if (computerSelection=="paper") {
+            return "You win! Scissors beats Paper";
+        } else {
+            return "You tie! Scissors ties Scissors.";
+        }
+    }
+}
+
+const playerSelection = "ScissorS";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
